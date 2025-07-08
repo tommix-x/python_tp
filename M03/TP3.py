d3ed6aafa92aeb8889e54e2faf66a947e1d23020
Tom CHARUEL <tom.charuel2023@campus-eni.fr>
2025-07-08 11:48:10 +0200

    modification, ajout de fonction dans M03-TP

--- M03/TP3.py
+++ M03/TP3.py
@@ -1,13 +1,24 @@
-def afficher_echiquier_sissa():
+def calculer_grains(n_lignes, n_colonnes):
     grains = 1
     total = 0
 
-    for ligne in range(1, 9):
-        for colonne in range(1, 9):
+    for ligne in range(1, n_lignes + 1):
+        for colonne in range(1, n_colonnes + 1):
             print(f"Ligne {ligne}, Colonne {colonne} : {grains} grain(s)")
             total += grains
             grains *= 2
 
-    print(f"\nTotal de grains de riz sur l'échiquier : {total}")
+    print(f"\nTotal de grains de riz sur un échiquier {n_lignes}x{n_colonnes} : {total}")
 
-afficher_echiquier_sissa()
+def main():
+    try:
+        lignes = int(input("Nombre de lignes : "))
+        colonnes = int(input("Nombre de colonnes : "))
+        if lignes <= 0 or colonnes <= 0:
+            print("Valeurs non valides")
+        else:
+            calculer_grains(lignes, colonnes)
+    except ValueError:
+        print("Entrée invalide")
+
+main()
